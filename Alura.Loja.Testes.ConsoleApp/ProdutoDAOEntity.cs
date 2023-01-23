1ca9f028d5b5fc6d010c2af2d4737f0008074b02
Samuca2003 <112135974+Samuca2003@users.noreply.github.com>
2023-01-23 16:59:06 -0300

    Add files via upload

---
+++ Alura.Loja.Testes.ConsoleApp/ProdutoDAOEntity.cs
@@ -0,0 +1,36 @@
+﻿using Alura.Loja.Testes.ConsoleApp;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+class ProdutoDAOEntity : IProdutoDAO, IDisposable
+{
+    private LojaContext contexto;
+    public void Adicionar(Produto p)
+    {
+        contexto.Produtos.Add(p);
+        contexto.SaveChanges();
+    }
+
+    public void Atualizar(Produto p)
+    {
+        contexto.Produtos.Update(p);
+        contexto.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        contexto.Dispose();
+    }
+
+    public IList<Produto> Produtos()
+    {
+        return contexto.Produtos.ToList();
+    }
+
+    public void Remover(Produto p)
+    {
+        contexto.Produtos.Remove(p);
+        contexto.SaveChanges();
+    }
+}
---
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var produtos = contexto.Produtos.ToList();
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                var novoProduto = new Produto
+                {
+                    Nome = "Desinfetante",
+                    Categoria = "Limpeza",
+                    Preco = 2.99
+                };
+                contexto.Produtos.Add(novoProduto);
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                contexto.SaveChanges();
+                ExibeEntries(contexto.ChangeTracker.Entries());
+            }
+        }
+
+        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        {
+            Console.WriteLine("===================");
+            foreach (var e in entries)
+            {
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+            }
+        }
+
+
+
+    }
+}
